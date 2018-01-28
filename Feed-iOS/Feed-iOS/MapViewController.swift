38ce3041fccb6b089580147e083f2f2854bbd48e
Varun-Ballari <varun@ballari.com>
2018-01-28 03:58:56 -0500

    adding all food banks

--- Feed-iOS/Feed-iOS/MapViewController.swift
+++ Feed-iOS/Feed-iOS/MapViewController.swift
@@ -32,9 +32,60 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             locationManager.startUpdatingLocation()
         }
         
-        // Do any additional setup after loading the view.
+        getBanks()
+        
     }
 
+    func getBanks() {
+        let urlstring = "https://feed-coc.herokuapp.com/allFoodBanks"
+        
+        let url = URL(string: urlstring)!
+        let session = URLSession.shared
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            guard let _: Data = data, let _: URLResponse = response, error == nil else {
+                return
+            }
+            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print(dataString)
+            
+            do {
+                if let data = data,
+                    let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                    let success = json["success"] as? Bool {
+                    if success {
+
+                        for i in (json["lat, lng"] as? [[Double]])! {
+                            let coordinate = CLLocationCoordinate2D(latitude: i[0], longitude: i[1])
+
+                            DispatchQueue.main.async {
+                                self.putOnMap(coordinate: coordinate)
+                            }
+                        }
+                        
+                    } else {
+                        
+                    }
+                }
+            } catch {
+                print("Error deserializing JSON: \(error)")
+            }
+        }
+        
+        task.resume()
+
+    }
+    
+    func putOnMap(coordinate: CLLocationCoordinate2D) {
+        let viewRegion = MKCoordinateRegionMakeWithDistance(coordinate, 2000, 2000)
+        self.map.setRegion(viewRegion, animated: false)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        self.map.addAnnotation(self.annotation)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         defer { currentLocation = locations.last }
         
